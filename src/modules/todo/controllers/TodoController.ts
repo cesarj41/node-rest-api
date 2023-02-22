243b1916cc13a97d2b74eb5b43ef5d489c035d02
Cesar Martinez <cesarj4114@gmail.com>
2023-02-21 23:05:32 -0400

    implemented endpoints

--- src/modules/todo/controllers/TodoController.ts
+++ src/modules/todo/controllers/TodoController.ts
@@ -1,54 +1,64 @@
-import { DbContext } from '@src/modules/data/DbContext';
-import { AppError } from '@src/util';
-import { JsonController, Param, Body, Get, Post, Put, Delete, NotFoundError } from 'routing-controllers';
-import { OpenAPI, ResponseSchema } from 'routing-controllers-openapi';
-import { Service, Inject } from 'typedi';
-import { CreateTodoModel } from '../models/CreateTodoModel';
-import { TodoModel } from '../models/TodoModel';
+import { DbContext } from "@src/modules/data/DbContext";
+import {
+  JsonController,
+  Param,
+  Body,
+  Get,
+  Post,
+  Put,
+  Delete,
+  NotFoundError,
+} from "routing-controllers";
+import { OpenAPI, ResponseSchema } from "routing-controllers-openapi";
+import { Service, Inject } from "typedi";
+import { CreateTodoModel } from "../models/CreateTodoModel";
+import { TodoModel } from "../models/TodoModel";
 
-@JsonController('/todos')
+@JsonController("/todos")
 @Service()
 export class TodoController {
-  @Inject()  
+  @Inject()
   private ctx: DbContext;
 
   @Get()
-  @OpenAPI({ summary: 'Return a list of todo' })
+  @OpenAPI({ summary: "Return a list of todo" })
   @ResponseSchema(TodoModel, { isArray: true })
   async getAll(): Promise<TodoModel[]> {
     const todos = await this.ctx.db.todo.findMany();
-    return todos.map(t => ({
+    return todos.map((t) => ({
       id: t.id.toString(),
       completed: t.completed,
       name: t.content,
     }));
   }
 
-  @Get('/:id')
-  @OpenAPI({ summary: 'Return todo by id' })
+  @Get("/:id")
+  @OpenAPI({ summary: "Return todo by id" })
   @ResponseSchema(TodoModel)
-  async getOne(@Param('id') id: number) {
-    const todo = await this.ctx.db.todo.findUnique({ where: { id }});
+  async getOne(@Param("id") id: number) {
+    const todo = await this.ctx.db.todo.findUnique({ where: { id } });
 
     if (!todo) {
       throw new NotFoundError("resource not found.");
     }
-    return { 
-      id, 
-      name: todo.content, 
-      completed: todo.completed, 
+    return {
+      id,
+      name: todo.content,
+      completed: todo.completed,
     };
   }
 
   @Post()
-  @OpenAPI({ summary: 'Creates a todo' })
+  @OpenAPI({ summary: "Creates a todo" })
   @ResponseSchema(TodoModel)
   async createTodo(@Body() todo: CreateTodoModel) {
-    const res = await this.ctx.db.todo.create({ data: {
-      content: todo.content,
-      completed: todo.completed ?? false,
-      authorId: "Cesar por ahora",
-    }});
+    const res = await this.ctx.db.todo.create({
+      data: {
+        content: todo.content,
+        completed: todo.completed ?? false,
+        authorId: "Cesar por ahora",
+      },
+    });
     return {
       id: res.id,
       content: res.content,
@@ -57,13 +67,13 @@ export class TodoController {
     };
   }
 
-  @Put('/:id')
-  put(@Param('id') id: number, @Body() user: any) {
-    return 'Updating a todo...';
+  @Put("/:id")
+  updateTodo(@Param("id") id: number, @Body() user: any) {
+    return "Updating a todo...";
   }
 
-  @Delete('/:id')
-  remove(@Param('id') id: number) {
-    return 'Removing todo...';
+  @Delete("/:id")
+  remove(@Param("id") id: number) {
+    return "Removing todo...";
   }
 }
--- src/util/error.ts
+++ src/util/error.ts
@@ -4,56 +4,6 @@ import { Response } from "express";
 import { HttpStatusCodes } from "@src/util";
 import { logger } from "../middlewares/Logger";
 import { HttpError } from "routing-controllers";
-
-export type ErrorType =
-  | "INVALID_REQUEST"
-  | "UNAUTHORIZE"
-  | "FORBIDDEN"
-  | "NOT_FOUND";
-
-export class AppError extends Error {
-  public readonly type: ErrorType;
-
-  constructor(type: ErrorType, description?: string) {
-    super(defaultMessage(type, description));
-
-    Object.setPrototypeOf(this, new.target.prototype);
-
-    this.type = type;
-
-    Error.captureStackTrace(this);
-  }
-}
-
-function defaultMessage(error: ErrorType, description?: string) {
-  if (error === "FORBIDDEN") {
-    return "You do not have permission to access this resource.";
-  }
-
-  if (error === "UNAUTHORIZE") {
-    return "Unauthorized request, please login.";
-  }
-
-  if (error === "NOT_FOUND") {
-    return "Resource not found";
-  }
-
-  return description || "";
-}
-
-function httpCode(error: ErrorType) {
-  switch (error) {
-    case "INVALID_REQUEST":
-      return HttpStatusCodes.BAD_REQUEST;
-    case "UNAUTHORIZE":
-      return HttpStatusCodes.UNAUTHORIZED;
-    case "FORBIDDEN":
-      return HttpStatusCodes.FORBIDDEN;
-    case "NOT_FOUND":
-    return HttpStatusCodes.NOT_FOUND;
-  }
-}
-
 class ErrorHandler {
   public handleError(
     log: typeof logger["logger"],
@@ -82,7 +32,7 @@ class ErrorHandler {
     }
   }
   public isTrustedError(error: Error) {
-    return error instanceof AppError;
+    return error instanceof HttpError;
   }
 }
 
